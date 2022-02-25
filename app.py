a7207dd19b6583859b379868101650473606467a
CodeNextPaco <paco@csedge.org>
2022-02-25 13:13:43 -0800

    added env variables

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request
+import requests
+from twilio.rest import Client
+
 
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def index():
@@ -14,11 +19,29 @@ def send_message():
     number = request.form["tel-input"]
     print(number)
 
+    #Adding a simple message
+    message = "I Love you bebe !! - Paco"
+    number = "+19252340221"
+    print(f"message: {message}")
+    #make the request
 
+    twilio_acct_id = "AC7dc9522a056142318ab0305ae59473dc"
+    twilio_phone_number = "+14143124358"
+    twilio_auth_token = "1e2fee921ef7a2e50a14373c372dbdeb"
+
+    client = Client(twilio_acct_id, twilio_auth_token)
+
+    message = client.messages.create(
+        to= number, 
+        from_= twilio_phone_number,
+        body= message)
+
+    print(f"Message id:  {message.sid}")
+  
 
     data = {
 
-        "success": "Message Sent"
+        "success": "Message Sent!!"
     }
     return render_template("index.html", data=data)
 
